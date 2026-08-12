6dd4465b5c7a7d532e9570372ae40d7ad379b51f
Jose Antonio García Yáñez <joseantoniogarciay@gmail.com>
2026-08-13 01:18:33 +0200

    fix(dev): use Scalar static configuration

--- infra/home/deploy-dev-web.sh
+++ infra/home/deploy-dev-web.sh
@@ -37,7 +37,6 @@ EXPO_NO_DOTENV=1 \
 api_docs_directory="$staging_directory/api-docs"
 install -d -m 755 "$api_docs_directory"
 install -m 644 infra/home/api-docs/index.html "$api_docs_directory/index.html"
-install -m 644 infra/home/api-docs/scalar-config.js "$api_docs_directory/scalar-config.js"
 install -m 644 contracts/openapi/v1/openapi.yaml "$api_docs_directory/openapi.yaml"
 
 mv "$staging_directory" "$release_directory"
